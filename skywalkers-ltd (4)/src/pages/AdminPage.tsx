b56985a559ac2d171761f963d6423c5182d7411d
josriri <phillipsmutwiri@gmail.com>
2026-07-03 23:19:44 +0300

    Delete skywalkers-ltd (4) directory

--- skywalkers-ltd (4)/src/pages/AdminPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "../lib/auth";
-import { AdminLogin } from "./AdminLogin";
-import { AdminDashboard } from "./AdminDashboard";
-
-export function AdminPage() {
-  const { token } = useAuth();
-  return token ? <AdminDashboard /> : <AdminLogin />;
-}
